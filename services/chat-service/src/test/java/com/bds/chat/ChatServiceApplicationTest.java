77617129f472c2198b6965c1c1e0c4049088629d
jnj3j3 <jnj22j33@gmail.com>
2026-07-01 08:00:09 +0000

    feature: 채팅 서비스 spring 구조 설정 및 ci 환경 테스트

---
+++ services/chat-service/src/test/java/com/bds/chat/ChatServiceApplicationTest.java
@@ -0,0 +1,12 @@
+package com.bds.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
