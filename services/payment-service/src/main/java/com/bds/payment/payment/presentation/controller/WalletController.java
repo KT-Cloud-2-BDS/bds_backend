86b6af4c6591f723115dab90ddc54a65d2b4466f
jaesung <barbo9510@icloud.com>
2026-07-04 15:00:40 +0900

    feat: Wallet 조회 API 경로 정정
    - GET /wallet -> GET /api/payment/wallet

--- services/payment-service/src/main/java/com/bds/payment/payment/presentation/controller/WalletController.java
+++ services/payment-service/src/main/java/com/bds/payment/payment/presentation/controller/WalletController.java
@@ -6,10 +6,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/payment")
 public class WalletController {
 
     private final WalletService walletService;
