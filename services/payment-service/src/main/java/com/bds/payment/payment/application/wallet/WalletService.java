4e2bf190f58171d59ad42e32d5e5113234208a18
jaesung <barbo9510@icloud.com>
2026-07-06 10:35:22 +0900

    rename: WalletResponse -> WalletResponseDto Rename - WalletResponse를 WalletResponseDto로 클래스명 변경 - WalletController, WalletService 참조 수정

--- services/payment-service/src/main/java/com/bds/payment/payment/application/wallet/WalletService.java
+++ services/payment-service/src/main/java/com/bds/payment/payment/application/wallet/WalletService.java
@@ -2,7 +2,7 @@ package com.bds.payment.payment.application.wallet;
 
 import com.bds.payment.payment.domain.wallet.Wallet;
 import com.bds.payment.payment.domain.wallet.WalletRepository;
-import com.bds.payment.payment.presentation.response.WalletResponse;
+import com.bds.payment.payment.presentation.response.WalletResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,9 +17,9 @@ public class WalletService {
     private final WalletCreator walletCreator;
 
     @Transactional(readOnly = true)
-    public WalletResponse getWallet(Long memberId) {
+    public WalletResponseDto getWallet(Long memberId) {
         Wallet wallet = walletRepository.findByMemberId(memberId)
                 .orElseGet(() -> walletCreator.createWalletSafely(memberId));
-        return WalletResponse.from(wallet);
+        return WalletResponseDto.from(wallet);
     }
 }
--- services/payment-service/src/main/java/com/bds/payment/payment/presentation/controller/WalletController.java
+++ services/payment-service/src/main/java/com/bds/payment/payment/presentation/controller/WalletController.java
@@ -1,7 +1,7 @@
 package com.bds.payment.payment.presentation.controller;
 
 import com.bds.payment.payment.application.wallet.WalletService;
-import com.bds.payment.payment.presentation.response.WalletResponse;
+import com.bds.payment.payment.presentation.response.WalletResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +17,7 @@ public class WalletController {
     private final WalletService walletService;
 
     @GetMapping("/wallet")
-    public ResponseEntity<WalletResponse> walletInfo(@RequestHeader("X-Member-Id") Long memberId) {
+    public ResponseEntity<WalletResponseDto> walletInfo(@RequestHeader("X-Member-Id") Long memberId) {
         return ResponseEntity.ok(walletService.getWallet(memberId));
     }
 }
