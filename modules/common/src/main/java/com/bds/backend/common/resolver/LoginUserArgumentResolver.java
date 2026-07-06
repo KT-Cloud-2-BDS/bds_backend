82357d27d2afd6d9bdefd0ddac783a05a928a31a
Yeojin <qhshqhsh3124287@gmail.com>
2026-07-06 16:36:08 +0900

    fix: 인증 resolver  예외 처리 수정

--- modules/common/src/main/java/com/bds/backend/common/resolver/LoginUserArgumentResolver.java
+++ modules/common/src/main/java/com/bds/backend/common/resolver/LoginUserArgumentResolver.java
@@ -3,10 +3,12 @@ package com.bds.backend.common.resolver;
 import com.bds.backend.common.annotation.LoginUser;
 import com.bds.backend.common.dto.CurrentUser;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.server.ResponseStatusException;
 
 public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
 
@@ -23,11 +25,14 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
         String email = webRequest.getHeader("X-User-Email");
         String role = webRequest.getHeader("X-User-Role");
 
-        if (userIdStr == null) {
-            throw new IllegalAccessException("인증된 유저 정보가 헤더에 존재하지 않습니다.");
+        Long userId;
+
+        try {
+            userId = Long.valueOf(userIdStr);
+        } catch (NumberFormatException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "유효하지 않은 사용자 ID 형식입니다.");
         }
 
-        Long userId = Long.valueOf(userIdStr);
         return new CurrentUser(userId, email, role);
     }
 
